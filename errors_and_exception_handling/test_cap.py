f32805e170de0a2e4710d5a425e02539b535d043
pawanglobal07 <pawanglobal07@gmail.com>
2023-07-19 11:44:55 +0200

    fitst commit

---
+++ errors_and_exception_handling/test_cap.py
@@ -0,0 +1,13 @@
+import unittest
+import cap
+
+class TestCap(unittest.TestCase):
+
+    def test_one_word(self):
+
+        text = 'python'
+        result = cap.cap_text(text)
+        self.assertEqual(result, 'Python')
+    
+if __name__ == '__main__':
+    unittest.main()
